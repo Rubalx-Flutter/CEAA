894b33da8cec79e38c794bd0c28697e242eae46a
Capi <54859381+Rubalx-Flutter@users.noreply.github.com>
2024-05-09 12:14:15 -0600

    Assets

--- Reto-Dashboard/tailwind.config.js
+++ Reto-Dashboard/tailwind.config.js
@@ -5,8 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "Lime-Green": "hsl(163, 72%, 41%)",
+        "Bright-Red": "hsl(356, 69%, 56%)",
+        "Facebook": "hsl(208, 92%, 53%)",
+        "Twitter": "hsl(203, 89%, 53%)",
+        "Instagram": "linear gradient hsl(37, 97%, 70%) to hsl(329, 70%, 58%)",
+        "YouTube": "hsl(348, 97%, 39%)",
+        
+        "Toggle": "hsl(230, 22%, 74%)",
+
+        "Very-Dark-Blue": "hsl(230, 17%, 14%)",
+        "Very-Dark-Blue-Top": "hsl(232, 19%, 15%)",
+        "Dark-Desaturated-Blue": "hsl(228, 28%, 20%)",
+        "Desaturated-Blue": "hsl(228, 34%, 66%)",
+
+        "White": "hsl(0, 0%, 100%)",
+
+        "Very-Pale-Blue-Top": "hsl(225, 100%, 98%)",
+        "Light-Grayish-Blue": "hsl(227, 47%, 96%)",
+        "Very-Dark-Blue": "hsl(230, 17%, 14%)",
+      }
+    },
   },
   plugins: [],
 }
-
